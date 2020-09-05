bada21daa399cb8933bc2a2dbcc9ca6bf6c5ecce
Eugene Esipov <zhenyaesipovm@mail.ru>
2020-09-05 19:03:16 +0400

    added page where admin may create new questionnaire

---
+++ frontend/mini-questionnaire-app/src/pages/QuestionnaireCreation.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="q-pa-md" v-if="!isCreated">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-input outlined v-model="title" label="Название анкеты" />
+        <q-btn label="Добавить новый вопрос" @click="addQuestion" outline color="primary"/>
+        <div
+          v-for="(question, questionIndex) in questions" :key="questionIndex"
+        >
+          <q-input outlined v-model="question.description" :label="questionIndex+1 + 'й вопрос'" />
+          <q-btn label="Добавить вариант ответа" @click="addAnswerOption(questionIndex)" outline color="primary"/>
+          <div
+            v-for="(answer, answerIndex) in question.answerOptions" :key="answerIndex"
+          >
+            <q-input outlined v-model="answer.description" :label="answerIndex+1 + 'й вариант ответа'" />
+          </div>
+        </div>
+        <div class="flex flex-center">
+          <q-btn label="Создать" type="submit" outline color="primary"/>
+        </div>
+      </q-form>
+      <div class="flex flex-center">
+        <q-btn to="/" label="Назад" outline color="primary" />
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <p>Анкета успешно создана!</p>
+    <div class="flex flex-center">
+      <q-btn to="/" label="Назад" outline color="primary" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionnaireCreation',
+  data () {
+    return {
+      isCreated: false,
+      title: '',
+      questions: [{
+        description: 'Первый вопрос',
+        answerOptions: [
+          { description: 'Первый вариант ответа' },
+          { description: 'Второй вариант ответа' }
+        ]
+      }]
+    }
+  },
+  methods: {
+    async onSubmit () {
+      const request = {
+        title: this.title,
+        questions: this.questions
+      }
+      const response = await fetch('http://localhost:8081/questionnaires', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + this.$store.getters['userStore/getToken']
+        },
+        body: JSON.stringify(request)
+      })
+
+      if (response.ok) {
+        this.isCreated = true
+      }
+    },
+    addAnswerOption (questionIndex) {
+      this.questions[questionIndex].answerOptions.push({ description: '' })
+    },
+    addQuestion () {
+      this.questions.push({
+        description: 'Новый вопрос',
+        answerOptions: [
+          { description: 'Первый вариант ответа' },
+          { description: 'Второй вариант ответа' }
+        ]
+      })
+    }
+  }
+}
+</script>
